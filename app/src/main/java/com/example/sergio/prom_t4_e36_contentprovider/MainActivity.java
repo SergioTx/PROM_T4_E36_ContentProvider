9e582b08cd2b2f180941c092a7377d424375c0c3
SergioTx <txitxo37@gmail.com>
2017-01-17 08:42:46 +0100

    primer commit: vacio

---
+++ app/src/main/java/com/example/sergio/prom_t4_e36_contentprovider/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.sergio.prom_t4_e36_contentprovider;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
